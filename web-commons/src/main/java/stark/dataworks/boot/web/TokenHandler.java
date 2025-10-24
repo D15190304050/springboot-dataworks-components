cd80071810ca627802cec9c62f214bc05d4bb1c5
DinoStark <15190304050@163.com>
2025-10-24 20:49:30 +0800

    getTokenFromRequest

--- web-commons/src/main/java/stark/dataworks/boot/web/TokenHandler.java
+++ web-commons/src/main/java/stark/dataworks/boot/web/TokenHandler.java
@@ -1,6 +1,8 @@
 package stark.dataworks.boot.web;
 
+import org.springframework.http.HttpCookie;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.util.StringUtils;
 
 import jakarta.servlet.http.Cookie;
@@ -18,7 +20,7 @@ public class TokenHandler
      * @param tokenCookieName
      * @return
      */
-    public static String getToken(HttpServletRequest request, String tokenCookieName)
+    public static String getTokenFromRequest(HttpServletRequest request, String tokenCookieName)
     {
         String token = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (StringUtils.hasText(token))
@@ -35,5 +37,30 @@ public class TokenHandler
         }
 
         return token;
+    }
+
+    private static String getTokenFromCookies(ServerHttpRequest request, String tokenCookieName)
+    {
+        if (!StringUtils.hasText(tokenCookieName))
+            return null;
+
+        HttpCookie cookie = request.getCookies().getFirst(tokenCookieName);
+        return cookie != null ? cookie.getValue() : null;
+    }
+
+    private static String getTokenFromHeaders(ServerHttpRequest request)
+    {
+        return request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+    }
+
+    /**
+     * Get token from request. Here the token in the header is preferred over that in the cookie.
+     * @param request
+     * @return
+     */
+    private static String getTokenFromRequest(ServerHttpRequest request, String tokenCookieName)
+    {
+        String tokenFromHeaders = getTokenFromHeaders(request);
+        return tokenFromHeaders != null ? tokenFromHeaders : getTokenFromCookies(request, tokenCookieName);
     }
 }
