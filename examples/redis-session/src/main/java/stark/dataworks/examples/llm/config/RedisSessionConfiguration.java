03be0d07086fe6a82030b5c444740a8d6e8e24fd
DinoStark <15190304050@163.com>
2026-01-05 22:47:05 +0800

    RedisChatContextManager

--- examples/redis-session/src/main/java/stark/dataworks/examples/llm/config/RedisSessionConfiguration.java
+++ examples/redis-session/src/main/java/stark/dataworks/examples/llm/config/RedisSessionConfiguration.java
@@ -5,14 +5,19 @@ import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import stark.dataworks.boot.llm.chat.AlibabaChatCompletionExecutor;
 import stark.dataworks.boot.llm.chat.ChatSessionFactory;
 
 @Configuration
 public class RedisSessionConfiguration
 {
+    @Autowired
+    private RedisTemplate<String, String> stringRedisTemplate;
+
     @Bean
     public ChatSessionFactory chatSessionFactory()
     {
@@ -36,8 +41,7 @@ public class RedisSessionConfiguration
             .build();
 
         AlibabaChatCompletionExecutor completionExecutor = new AlibabaChatCompletionExecutor(chatClient);
-        ChatSessionFactory chatSessionFactory = new ChatSessionFactory(completionExecutor);
 
-        return chatSessionFactory;
+        return new ChatSessionFactory(completionExecutor, stringRedisTemplate, null);
     }
 }
